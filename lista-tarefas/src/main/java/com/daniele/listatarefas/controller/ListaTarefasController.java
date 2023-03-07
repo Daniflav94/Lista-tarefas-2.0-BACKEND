d38029883fed0bd95406bb9b1c7a6785d2cb07e8
Daniflav94 <danyelly_mell@hotmail.com>
2023-03-07 13:07:06 -0300

    Controller de Lista concluído

---
+++ lista-tarefas/src/main/java/com/daniele/listatarefas/controller/ListaTarefasController.java
@@ -0,0 +1,63 @@
+package com.daniele.listatarefas.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.daniele.listatarefas.dto.ListaTarefasDTO;
+import com.daniele.listatarefas.model.ListaTarefas;
+import com.daniele.listatarefas.service.ListaTarefasService;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+@Validated
+@RestController
+@RequestMapping("/api/lista")
+public class ListaTarefasController {
+
+    private final ListaTarefasService listaTarefasService;
+
+    public ListaTarefasController(ListaTarefasService listaTarefasService) {
+        this.listaTarefasService = listaTarefasService;
+    }
+
+    @GetMapping
+    public List<ListaTarefas> listar() {
+        return listaTarefasService.listar();
+    }
+
+    @GetMapping("/{id}")
+    public ListaTarefas listarPorId(@PathVariable @NotNull @Positive Long id) {
+        return listaTarefasService.listarPorId(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public ListaTarefas criar(@RequestBody @Valid ListaTarefasDTO listaDTO) {
+        return listaTarefasService.criar(listaDTO);
+    }
+
+    @PutMapping("/{id}")
+    public ListaTarefas editar(@PathVariable @NotNull @Positive Long id, @Valid @RequestBody ListaTarefasDTO listaDTO) {
+        return listaTarefasService.editar(id, listaDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void deletar(@PathVariable @NotNull @Positive Long id) {
+        listaTarefasService.deletar(id);
+    }
+    
+}

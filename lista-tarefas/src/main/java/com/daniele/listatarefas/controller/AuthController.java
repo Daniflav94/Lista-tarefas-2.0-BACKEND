2ff9c10284d704be912ad76cf3e0a93be5cd07ee
Daniflav94 <danyelly_mell@hotmail.com>
2023-03-14 16:12:10 -0300

    Rota adicionada em auth controller

--- lista-tarefas/src/main/java/com/daniele/listatarefas/controller/AuthController.java
+++ lista-tarefas/src/main/java/com/daniele/listatarefas/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.daniele.listatarefas.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.daniele.listatarefas.dto.CredenciaisDTO;
@@ -11,6 +12,7 @@ import com.daniele.listatarefas.service.AuthService;
 import jakarta.validation.Valid;
 
 @RestController
+@RequestMapping("/api/auth/login")
 public class AuthController {
 
     private AuthService authService;
@@ -19,7 +21,7 @@ public class AuthController {
         this.authService = authService;
     }
 
-    @PostMapping("/auth/login")
+    @PostMapping
     public TokenDTO login(@Valid @RequestBody CredenciaisDTO dto) {
         return authService.login(dto);
     }
